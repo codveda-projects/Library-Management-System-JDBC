d9bcbfd0e7d534e0185c06b61edc2c84539b2b97
Thavha Rambau <198281130+ThavhaRambau@users.noreply.github.com>
2026-06-25 02:56:38 +0200

    Update DBConnection URL to disable SSL for MySQL connection

--- DBConnection.java
+++ DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db?useSSL=false";
     private static final String USER = "javauser";
     private static final String PASSWORD = "mypassword";
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Library-Management-System-JDBC
 a Library Management System using Java and JDBC for database connectivity. The system should allow adding, borrowing, and returning books.
+
+# compile and run
+javac -cp ".;mysql-connector-java-5.1.42-bin.jar" *.java 
+
+java -cp ".;mysql-connector-java-5.1.42-bin.jar" Main
+
+
+
